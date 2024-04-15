6c8da46d41b1b92c4e39d2f649511effb0e8aeaf
unknown <swanandkulkarni0007@gmail.com>
2024-04-15 08:04:56 +0530

    Feat:New Model

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import joblib
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+st.title(" Diabetes_Prediction ")
+
+@st.cache_resource
+def load_util():
+    model = joblib.load(r"diabModel.pkl")
+    return model
+
+g = st.selectbox(" Enter your gender " , ("Male" , "Female"))
+
+if(g == "Female"):
+    gen = 0
+else:
+    gen = 1
+
+age = st.number_input("Enter your age")
+h = st.selectbox("Weather you are suffering from hypertension" , ("No" , "Yes"))
+if(h == "No"):
+    hyp = 0
+else:
+    hyp = 1
+
+hd = st.selectbox("Weather you are suffering from any heart_disease" , ("Yes" , "No"))
+if(hd == "Yes"):
+    hdis = 1
+else:
+    hdis = 0
+bmi = st.number_input("Enter your BMI")
+hb = st.number_input("Enter your HbA1c_level")
+glu = st.number_input("Enter your blood_glucose_level")
+
+button = st.button("Predict")
+
+if(button):
+    model = load_util()
+    ans = model.predict([[gen,age,hyp,hdis,bmi,hb,glu]])
+    if(ans == 1):
+        st.write(" # You are suffering from Diabetes")
+    else:
+        st.write(" # You are not suffering from Diabetes")
+else:
+    st.write("Check the Prediction")
